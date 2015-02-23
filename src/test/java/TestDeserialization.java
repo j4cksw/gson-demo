69365b9d8a011978e6db01b76b6f7451a3029411
Prayoch Rujira <jay@odd-e.co.th>
2015-02-23 14:28:28 +0700

    add matchers utility

--- src/test/java/TestDeserialization.java
+++ src/test/java/TestDeserialization.java
@@ -1,5 +1,14 @@
-/**
- * Created by JackSW on 2/23/15 AD.
- */
+import com.google.gson.Gson;
+import com.nitorcreations.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+
 public class TestDeserialization {
+    @Test
+    public void TestDeserializeToOject(){
+        Gson gson = new Gson();
+
+        Assert.assertThat(gson.fromJson("{\"name\":\"Jack\"}", User.class), Matchers.reflectEquals(new User("Jack")));
+    }
 }
